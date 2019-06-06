a27fe8cec9c9464f0223e62850cfdadc07b029ea
Kai Fabi <33313471+KaiFabi@users.noreply.github.com>
2019-06-06 19:34:08 +0200

    Add files via upload

---
+++ data.py
@@ -0,0 +1,50 @@
+import os
+import struct
+import numpy as np
+
+def extract_data(img_path, lbl_path):
+    with open(lbl_path, 'rb') as flbl:
+        magic, num = struct.unpack(">II", flbl.read(8))
+        lbl_data = np.fromfile(flbl, dtype=np.int8)
+
+    with open(img_path, 'rb') as fimg:
+        magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
+        img_data = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl_data), rows, cols)
+    return img_data, lbl_data
+
+def normalize(X):
+    return 2.*(X/255.-0.5)
+
+def get_data(dataset, norm, one_hot):
+
+    path = "./data/" + dataset
+    file_path_train_img = os.path.join(path, 'train-images-idx3-ubyte')
+    file_path_train_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
+    file_path_test_img = os.path.join(path, 't10k-images-idx3-ubyte')
+    file_path_test_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
+    
+    # Extract data from files
+    img_train, lbl_train = extract_data(file_path_train_img, file_path_train_lbl)
+    img_test, lbl_test = extract_data(file_path_test_img, file_path_test_lbl)
+
+    # Normalize image data
+    if norm:
+        img_train, img_test = normalize(img_train), normalize(img_test)
+
+    # Reshape images to vectors
+    n_input = img_train.shape[-1]**2
+    img_train, img_test = img_train.reshape(len(img_train), n_input), img_test.reshape(len(img_test), n_input)
+
+    # Compute one-hot labels
+    n_classes = len(np.unique(lbl_train))
+    if one_hot:
+        lbl_train = np.eye(n_classes)[lbl_train]
+        lbl_test = np.eye(n_classes)[lbl_test]
+
+    # Split evaluation dataset from training dataset
+    prop = 0.95 # 5% of training data will be used for evaluation
+    split_idx = int(prop*len(img_train))
+    img_eval, lbl_eval = img_train[split_idx:], lbl_train[split_idx:]
+    img_train, lbl_train = img_train[:split_idx], lbl_train[:split_idx]
+
+    return img_train, lbl_train, img_eval, lbl_eval, img_test, lbl_test, n_classes, n_input
---
+++ mlp.py
@@ -0,0 +1,158 @@
+import os
+import data
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import expit # numerically stable sigmoid function
+
+# Seed
+np.random.seed(seed=73214)
+
+# Normalize data
+def normalize(X):
+    X_min = np.min(X, axis=1, keepdims=True)
+    X_max = np.max(X, axis=1, keepdims=True)
+    return (X - X_min) / (X_max - X_min)
+
+# Activation functions
+def sigma(x):
+    return expit(x)
+
+def sigma_prime(x):
+    u = sigma(x)
+    return u-u*u
+
+def relu(x):
+    return np.maximum(x, 0.)
+
+def relu_prime(x):
+    return (x>0).astype(x.dtype)
+
+# Xavier weight initialization
+def xavier_init(in_size, out_size):
+    return np.sqrt(6./(in_size+out_size))
+
+# Multilayer Perceptron Class
+class NeuralNetwork(object):
+
+    def __init__(self, network_config):
+
+        self.network_depth = len(network_config)
+        self.network_config = network_config
+
+        # Weights 
+        self.W = [np.random.uniform(-xavier_init(network_config[l],network_config[l+1]), \
+                xavier_init(network_config[l],network_config[l+1]), \
+                size=(network_config[l+1],network_config[l])) for l in range(self.network_depth-1)]
+        # Bias
+        self.b = [0.1*np.ones((network_config[l],1)) for l in range(1,self.network_depth)]
+        # Pre-activation
+        self.z = [np.zeros((self.network_config[l],1)) for l in range(1,self.network_depth)]
+        # Activations
+        self.a = [np.zeros((self.network_config[l],1)) for l in range(self.network_depth)]
+        # Gradients
+        self.dW = [np.zeros((self.network_config[l+1],self.network_config[l])) for l in range(self.network_depth-1)] 
+        self.db = [np.zeros((self.network_config[l],1)) for l in range(1,self.network_depth)]
+
+
+    def grouped_rand_idx(self, n_total, batch_size):
+        idx = np.random.permutation(n_total)
+        return [idx[i:i+batch_size] for i in range(0, len(idx), batch_size)]
+
+
+    def optimize(self, x_train, y_train, x_eval, y_eval, x_test, y_test, epochs, batch_size, learning_rate):
+         
+        eta = learning_rate/batch_size
+        for epoch in range(epochs):
+            idx_list = self.grouped_rand_idx(len(x_train), batch_size)
+            for idx in idx_list:
+                # Get batch of random training samples
+                x_batch, y_batch = x_train[idx], y_train[idx]
+                # Feedforward pass
+                self.feedforward(x_batch) 
+                # Backpropapagation with gradient descent
+                self.backprop_gradient_descent(y_batch, eta)
+ 
+            if epoch % 1 == 0:
+                self.prediction(x_eval, y_eval, epoch, mode="eval")
+
+            if epoch % 30 == 0:
+                self.visualize_weights(epoch)
+
+        # Compute test accuracy and loss
+        self.prediction(x_test, y_test, epoch, mode="test")
+
+
+    def backprop_gradient_descent(self, Y, eta):
+        # Backpropagation
+        delta = (self.a[-1] - Y) * sigma_prime(self.z[self.network_depth-2])
+        self.dW[self.network_depth-2] += delta.T.dot(self.a[self.network_depth-2])
+        self.db[self.network_depth-2] += np.sum(delta.T, axis=1, keepdims=True)
+
+        for l in reversed(range(self.network_depth-2)):
+            delta = delta.dot(self.W[l+1]) * relu_prime(self.z[l])
+            self.dW[l] += self.a[l].T.dot(delta).T
+            self.db[l] += np.sum(delta.T, axis=1, keepdims=True)
+
+        # Gradient descent: Update Weights and Biases
+        for l in range(self.network_depth-1):
+            self.W[l] += - eta * self.dW[l]
+            self.b[l] += - eta * self.db[l]
+
+        # Reset gradients
+        self.dW = [np.zeros_like(self.dW[l]) for l in range(self.network_depth-1)]
+        self.db = [np.zeros_like(self.db[l]) for l in range(self.network_depth-1)]
+
+
+    def feedforward(self, X):
+        # Feedforward
+        self.a[0] = X 
+        for l in range(self.network_depth-2):
+            self.z[l] = self.a[l].dot(self.W[l].T) + self.b[l].T
+            self.a[l+1] = relu(self.z[l])
+        self.z[-1] = self.a[-2].dot(self.W[-1].T) + self.b[-1].T
+        self.a[-1] = sigma(self.z[-1])
+        #return z, a
+
+
+    def pred(self, X, Y):
+        neurons = X
+        for l in range(self.network_depth-2):
+            neurons = relu(neurons.dot(self.W[l].T) + self.b[l].T)
+        logits = neurons.dot(self.W[-1].T) + self.b[-1].T
+        accuracy = (np.argmax(logits, axis=1) == np.argmax(Y, axis=1)).sum() / len(X)
+        loss = np.sum((Y - sigma(logits))**2) / len(X)
+        return loss, accuracy
+
+
+    def prediction(self, X, Y, epoch, mode):
+        loss, accuracy = self.pred(X, Y)
+        print('epoch {1} {0}_loss {2:.6f} {0}_accuracy {3:.4f}'.format(mode, epoch, loss, accuracy), flush=True)
+
+
+    def visualize_weights(self, epoch):
+        nrow, ncol = 8, 8
+        fig, axes = plt.subplots(nrows = nrow, ncols=ncol, figsize=(ncol, nrow))
+        for k, ax in enumerate(axes.flatten()):
+            ax.imshow(self.W[0][k].reshape(28,28), cmap="viridis")
+            ax.axis("off")
+        plt.subplots_adjust(left=0.02, bottom=0.02, right=0.98, top=0.98, wspace=0.02, hspace=0.02)
+        plt.savefig('mlp_weights_epoch_{}.png'.format(epoch), dpi=250)
+        plt.close()
+
+
+# Load data
+x_train, y_train, x_eval, y_eval, x_test, y_test, n_classes, n_input = data.get_data(dataset="mnist", norm=True, one_hot=True)
+
+# Training parameters
+learning_rate = 0.05
+batch_size = 64
+epochs = 1000
+
+# Network configuration
+network_config = [n_input, 128, n_classes]
+
+# Initialize network
+network = NeuralNetwork(network_config)
+
+# Start training
+network.optimize(x_train, y_train, x_eval, y_eval, x_test, y_test, epochs, batch_size, learning_rate)
